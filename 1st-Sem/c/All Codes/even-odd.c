883edda3b58c28477c7040e01494f16902b17ad2
Manthan Sahu <manthansahusahu@gmail.com>
2026-07-29 19:31:38 +0530

    Create even-odd.c

---
+++ 1st-Sem/c/All Codes/even-odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ void main()
+{
+  int num;
+  printf("enter the value pf a:");
+  scanf("%d",&a);
+  if(num == 0)
+    {
+      printf("%d is not an odd and even number");
+    }
+  else if(num % 2==0)
+    {
+      printf("%d is an even number");
+  else 
+    {
+      printf("%d is an odd number");
+    }
+}
+
