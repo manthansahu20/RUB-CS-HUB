8c5240c3f8ebc0062ac74932412a2738cc790df4
Manthan Sahu <manthansahusahu@gmail.com>
2026-07-30 14:58:27 +0530

    Add Check_collinear.py to determine collinearity

---
+++ 1st-Sem/Python/All-Codes/Check_collinear.py
@@ -0,0 +1,15 @@
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+x3 = int(input("Enter x3: "))
+y3 = int(input("Enter y3: "))
+
+area = x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)
+
+if area == 0:
+    print("The points are Collinear.")
+else:
+    print("The points are NOT Collinear.")
