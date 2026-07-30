f5738e15d9149a766cfb73a3213047be7f4db219
Manthan Sahu <manthansahusahu@gmail.com>
2026-07-30 15:04:08 +0530

    Add Variable datatype demonstration in Python

---
+++ 1st-Sem/Python/All-Codes/Variable _datatype.py
@@ -0,0 +1,14 @@
+a = 10          # Integer
+b = 12.5        # Float
+c = "Python"    # String
+d = True        # Boolean
+
+print("Integer:", a)
+print("Float:", b)
+print("String:", c)
+print("Boolean:", d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
