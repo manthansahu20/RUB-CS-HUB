ed36e717e7971d50636a8a7a866633b5f6217c07
Manthan Sahu <manthansahusahu@gmail.com>
2026-07-30 14:55:11 +0530

    Add quadratic equation root finder
    Implement a program to find roots of a quadratic equation based on user input for coefficients a, b, and c. Handles real and imaginary roots.

---
+++ 1st-Sem/Python/All-Codes/Roots_of_eqn.py
@@ -0,0 +1,19 @@
+import math
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+d = b*b - 4*a*c
+
+if d > 0:
+    r1 = (-b + math.sqrt(d)) / (2*a)
+    r2 = (-b - math.sqrt(d)) / (2*a)
+    print("Roots are:", r1, r2)
+
+elif d == 0:
+    r = -b / (2*a)
+    print("Equal Roots:", r)
+
+else:
+    print("Imaginary Roots")
