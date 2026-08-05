49446aba0b62e1bc82012afd8f0b1ec66e4524c5
Manthan Sahu <manthansahusahu@gmail.com>
2026-08-05 11:28:54 +0530

    Create Selection_sort.c

---
+++ 2nd-Sem/DSA/All-Codes/Selection_sort.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i,j,min,t;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    printf("Enter elements: ");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    for(i=0;i<n-1;i++)
+    {
+        min=i;
+        for(j=i+1;j<n;j++)
+            if(a[j]<a[min])
+                min=j;
+
+        t=a[i];
+        a[i]=a[min];
+        a[min]=t;
+    }
+
+    printf("Sorted Array: ");
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+
+    return 0;
+}
